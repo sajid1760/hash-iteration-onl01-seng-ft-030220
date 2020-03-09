33913c8846856941cb1f9a2468bd37b76a4d3ad4
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 21:05:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,13 @@
 
 def happy_birthday(birthday_kids)
    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+
+def age_appropriate_birthday(birthday_kids)
+   birthday_kids.each do |kids_name, age|
     if age < 12 then
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
@@ -13,5 +20,3 @@ def happy_birthday(birthday_kids)
     end
   end
 end
-
-#happy_birthday(birthday_kids)
